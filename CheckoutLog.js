e74e1ab59c5aa59f818c514a64f926672eb7948d
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-07-06 13:54:15 -0400

    update Checkout Log

--- CheckoutLog.js
+++ CheckoutLog.js
@@ -3,42 +3,31 @@
 // CHECKOUT LOG CLASS
 // ===============================================================================
 class CheckoutLog {
-  constructor(data) {
-    this.timestamp = data.timestamp || new Date();
-    this.emailAddress = data.emailAddress;
-    this.bikeCode = data.bikeCode;
-    this.confirmBikeCode = data.confirmBikeCode;
-    this.keyAvailableCheck = data.keyAvailableCheck;
-    this.conditionConfirmation = data.conditionConfirmation;
-    this.db = new DatabaseManager();
-  }
-
-  static fromFormResponse(e) {
+  constructor(e) {
     const sheet = e.source.getActiveSheet();
     const range = e.range;
     //  Get the last row with data in the edited sheet
     const lastRowId = sheet.getLastRow();
 
     const responses = sheet.getRange(lastRowId, 1, 1, sheet.getLastColumn()).getValues()[0];
-    return new CheckoutLog({
-      timestamp: responses[0],
-      emailAddress: responses[1],
-      bikeCode: responses[2],
-      confirmBikeCode: responses[3],
-      keyAvailableCheck: responses[4],
-      conditionConfirmation: responses[5]
-    });
+    
+    this.timestamp = responses[0];
+    this.emailAddress = responses[1];
+    this.bikeCode = responses[2];
+    this.confirmBikeCode = responses[3];
+    this.keyAvailableCheck = responses[4];
+    this.conditionConfirmation = responses[5];
+    this.db = new DatabaseManager();
   }
 
   validate() {
     if (this.bikeCode !== this.confirmBikeCode) {
+      Logger.log('Bike code confirmation does not match')
       throw new Error('Bike code confirmation does not match');
     }
     if (this.keyAvailableCheck !== 'Yes') {
+      Logger.log('User did not confirm key availability')
       throw new Error('User did not confirm key availability');
-    }
-    if (this.conditionConfirmation !== 'Yes') {
-      throw new Error('User did not confirm bike condition');
     }
   }
 }
