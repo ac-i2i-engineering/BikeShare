92871b26b94e91a38cefd4add05b089f6c1f0efb
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-07-05 21:05:36 -0400

    add bike codes to config

--- CONFIG.js
+++ CONFIG.js
@@ -12,5 +12,6 @@ const CONFIG = {
   FORMS: {
     CHECKOUT_FORM_ID: '1p5g05E08NOfTUvPzMIfu7LNruCq9Zd49I2jt4Ly4Drs',
     RETURN_FORM_ID: '1VFAY-49Qx2Ob5OdVZkI2rH9xbaT0DRF63q6fWZh-Pbc'
-  }
+  },
+  BIKE_NAMES:['Gates','Harris','Hitchcock','Humphrey','Meiklejohn','Moore','Olds','Seelye','Stearns']
 };
