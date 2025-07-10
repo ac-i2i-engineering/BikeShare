62b6ea23a71e3745ae04e364509993768ce46591
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-07-10 00:01:23 -0400

    improve variables naming

--- tests/Utils.js
+++ tests/Utils.js
@@ -8,10 +8,10 @@ function printFormFieldInfo(formId){
 function simulateHandleOnFormSubmit(sheetName, response) {
   const service = new BikeShareService();
   // Check if the edit is in the 'Checkout Logs' or 'Return Logs' sheet
-  if (sheetName === CONFIG.SHEETS.CHECKOUT_LOGS) {
+  if (sheetName === CONFIG.SHEETS.CHECKOUT_LOGS.NAME) {
     const result = service.processCheckout(response);
     Logger.log('Checkout processed:', result.message || result.error);
-  } else if (sheetName === CONFIG.SHEETS.RETURN_LOGS) {
+  } else if (sheetName === CONFIG.SHEETS.RETURN_LOGS.NAME) {
     const result = service.processReturn(response);
     Logger.log('Return processed:', result.message || result.error);
   }
