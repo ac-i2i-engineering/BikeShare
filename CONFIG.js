957c87304b8a150d84114f53fd62f63bc8e0b5af
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-07-11 00:11:53 -0400

    add: added Bike_Status sheet's dropdown column options

--- CONFIG.js
+++ CONFIG.js
@@ -4,11 +4,39 @@
 const CONFIG = {
   DEBUG_MODE: true,
   SHEETS: {
-    BIKES_STATUS: {NAME:'Bikes Status',SORT_COLUMN: 0, SORT_ORDER: 'asc',ERROR_FLAG_COLOR: '#f9cb9c'},
-    USER_STATUS: {NAME:'User Status', SORT_COLUMN: 0, SORT_ORDER: 'asc',ERROR_FLAG_COLOR: '#f9cb9c'},
-    CHECKOUT_LOGS: {NAME:'Checkout Logs', SORT_COLUMN: 0, SORT_ORDER: 'desc',ERROR_FLAG_COLOR: '#f9cb9c'},
-    RETURN_LOGS: {NAME:'Return Logs', SORT_COLUMN: 0, SORT_ORDER: 'desc',ERROR_FLAG_COLOR: '#f9cb9c'},
-    REPORTS: {NAME:'Reports', SORT_COLUMN: 0, SORT_ORDER: 'desc',ERROR_FLAG_COLOR: '#f9cb9c'}
+    BIKES_STATUS: {
+      NAME:'Bikes Status',
+      SORT_COLUMN: 0, 
+      SORT_ORDER: 'asc',
+      ERROR_FLAG_COLOR: '#f9cb9c',
+      AVAILABILITY_COL_OPTIONS: ['Available', 'Checked Out', 'Out of Service'],
+      MAINTENANCE_COL_OPTIONS: ['Good', 'In Repair', 'Has Issues', 'Missing'],
+      SIZE_COL_OPTIONS: ['S', 'M', 'L', 'XL'],
+    },
+    USER_STATUS: {
+      NAME:'User Status',
+      SORT_COLUMN: 0,
+      SORT_ORDER: 'asc',
+      ERROR_FLAG_COLOR: '#f9cb9c',
+    },
+    CHECKOUT_LOGS: {
+      NAME:'Checkout Logs',
+      SORT_COLUMN: 0,
+      SORT_ORDER: 'desc',
+      ERROR_FLAG_COLOR: '#f9cb9c',
+    },
+    RETURN_LOGS: {
+      NAME:'Return Logs',
+      SORT_COLUMN: 0,
+      SORT_ORDER: 'desc',
+      ERROR_FLAG_COLOR: '#f9cb9c'
+    },
+    REPORTS: {
+      NAME:'Reports',
+      SORT_COLUMN: 0,
+      SORT_ORDER: 'desc',
+      ERROR_FLAG_COLOR: '#f9cb9c'
+    }
   },
   FORMS: {
     CHECKOUT_FORM_ID: '1ThxJFJLjtQkvzXuX7ZPa2vEYWzIokWK89GUbW507zpM',
