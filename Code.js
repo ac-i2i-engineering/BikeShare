2b51c6b5cc92ecbda9d69d846cc5d2be4a8d4d0f
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-07-12 22:18:06 -0400

    refactor: move sort checkoutLog and returnLogs from bikeShareService

--- Code.js
+++ Code.js
@@ -14,8 +14,10 @@ function handleOnFormSubmit(e) {
   // Check if the edit is in the 'Checkout Logs' or 'Return Logs' sheet
   if (sheetName === CONFIG.SHEETS.CHECKOUT_LOGS.NAME) {
     service.processCheckout(responses, range);
+    service.db.sortByColumn(null, CONFIG.SHEETS.CHECKOUT_LOGS.NAME);
   } else if (sheetName === CONFIG.SHEETS.RETURN_LOGS.NAME) {
     service.processReturn(responses, range);
+    service.db.sortByColumn(null, CONFIG.SHEETS.RETURN_LOGS.NAME);
   }
 }
 
