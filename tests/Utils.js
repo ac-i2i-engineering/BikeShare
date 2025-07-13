9d23beaba886a7592afddcfc0f433770075e7ea8
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-07-12 22:37:34 -0400

    create: levenshteinDistance & fuzzyMatchString fncs

--- tests/Utils.js
+++ tests/Utils.js
@@ -39,3 +39,34 @@ function deleteAllTriggers(){
     ScriptApp.deleteTrigger(triggers[i]); 
     }
 }
+
+function levenshteinDistance(str1, str2) {
+  const matrix = Array(str2.length + 1).fill().map(() => Array(str1.length + 1).fill(0));
+  for (let i = 0; i <= str1.length; i++) matrix[0][i] = i;
+  for (let j = 0; j <= str2.length; j++) matrix[j][0] = j;
+  for (let j = 1; j <= str2.length; j++) {
+    for (let i = 1; i <= str1.length; i++) {
+      const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
+      matrix[j][i] = Math.min(
+        matrix[j][i - 1] + 1,
+        matrix[j - 1][i] + 1,
+        matrix[j - 1][i - 1] + cost
+      );
+    }
+  }
+  return matrix[str2.length][str1.length];
+}
+
+function fuzzyMatchString(targetStr, compStr){
+  if (!targetStr || !compStr) return false;
+  if (targetStr.toLowerCase() === compStr.toLowerCase()) return true;
+  if (targetStr.length < 3 || compStr.length < 3) return false;
+  const distance = levenshteinDistance(targetStr, compStr);
+  const maxLen = Math.max(targetStr.length, compStr.length);
+  return distance / maxLen < CONFIG.FUZZY_MATCHING_THRESHOLD;
+}
+
+function quickTest(){
+  const len = levenshteinDistance('hello', 'hallo');
+  console.log(len)
+}
